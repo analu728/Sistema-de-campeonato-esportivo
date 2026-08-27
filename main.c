ae47fd175077d89a667679e99bb69de94d8a3e6b
felipebarros7202 <felipebarros7202@gmail.com>
2026-08-27 15:06:19 -0300

    Update main.c

--- main.c
+++ main.c
@@ -6,12 +6,28 @@
 #include "utils.h"
 
 int main(void) {
-
+    int opcao, b;
     VetTimes vt;
-
     inicializarVetTimes(&vt);
-    printf("Capacidade: %d, Quantidade: %d\n", vt.cap, vt.qtd);
+    do{
+        printf("Sistema de Campeonato Esportivo\n");
+    printf("Escolha uma opcao:\nCadastrar time: 1\nListar times: 2\nAlterar dados de um time: 3\n");
+    scanf(" %d", &opcao);
+    switch(opcao){
+        case 1:
+            cadastrarTime(&vt);
+            break;
+        case 2:
+            listarTimes(&vt);
+            break;
+        case 0:
+            printf("Encerrando");
+            break;
+        default:
+            printf("Opcao invalida.");
+            break;
+     }
+    }while(opcao !=0);
     liberarVetTimes(&vt);
-    
-return 0;
+    return 0;
 }
