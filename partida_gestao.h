5b28ee7ecbd2eb7d550d46a8dd6299bb3aafa825
felipebarros7202 <felipebarros7202@gmail.com>
2026-08-30 15:43:21 -0300

    Update partida_gestao.h

--- partida_gestao.h
+++ partida_gestao.h
@@ -3,6 +3,12 @@
 
 #include "tipos.h"
 
-// os protótipos das funções vêm aqui embaixo
+void inicializarVetPartidas(VetPartidas *vp);
+void cadastrarPartida(VetPartidas *vp, const VetTimes *vt);
+void atualizarPartida(VetPartidas *vp, int id);
+void removerPartida(VetPartidas *vp, int id);
+void registrarResultado(VetPartidas *vp, VetTimes *vt);
+void exibirChaveamentoMataMata(const VetPartidas *vp, const VetTimes *vt);
+void liberarVetPartidas(VetPartidas *vp);
 
 #endif
