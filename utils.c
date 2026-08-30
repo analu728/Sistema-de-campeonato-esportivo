03c36a868a86ca4bfc48bc4f1790fd51ff5ed65b
felipebarros7202 <felipebarros7202@gmail.com>
2026-08-30 15:41:54 -0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,1 +1,45 @@
 #include "utils.h"
+#include <string.h>
+#include <stdio.h>
+
+int BuscarTime(const VetTimes *vt, int id){
+    for(int i=0;i<vt->qtd;i++){
+        if(vt->itens[i].id==id){
+            return i;//achou
+        }
+    }
+    return -1;
+}
+
+int BuscarPartida(const VetPartidas *vp, int id){
+    for(int i=0;i<vp->qtd;i++){
+        if(vp->itens[i].id==id){
+            return i;//achou
+        }
+    }
+    return -1;
+}
+
+int validardata(Data d){
+    if(d.mes<1 || d.mes>12) return 0;
+    if(d.dia<1 || d.dia>31) return 0;
+    if(d.ano<2000) return 0;
+
+    if ((d.mes == 4||d.mes == 6 || d.mes == 9 || d.mes == 11) && d.dia > 30) {
+        return 0;
+    }
+    if (d.mes == 2 && d.dia > 29) {
+        return 0;
+    }
+    return 1;
+}
+
+int validarplacar(int gols){
+    return gols>=0;
+}
+int validarnome(const char *nome) {
+    if (nome==NULL||strlen(nome) == 0) {
+        return 0;
+    }
+    return 1;
+}
